0008e139f56fe56aa78706c5e40a76dc29681cd4
Cozark <thaithangpc@gmail.com>
2021-11-17 16:23:56 +0700

    first release

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+
+    public static class Program
+    {
+
+        public static void Main()
+        {
+            ListBook a = new ListBook();
+            book temp = new book();
+            temp.maSach = "001";
+            temp.tenSach = "Nhà giả kim";
+            temp.TacGia = "Quốc Trung";
+            temp.ChuDe = "Triết học";
+            temp.NXB = "Kim Đồng";
+            temp.price = 290000;
+            temp.vt = "A-03";
+            temp.imgSrc = "C:/Users/Thai_Long/Downloads/giakim.jpg";
+            temp.NgXB = new DateTime(2019, 1, 1);
+            a.Push(temp);
+            a.Display();
+            Console.ReadKey();
+        }
+    }
+}
